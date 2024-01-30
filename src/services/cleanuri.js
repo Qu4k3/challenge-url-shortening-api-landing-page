eecf652f76882b0914780b089389260deabef0b8
Qu4k3 <paul.guillamon@gmail.com>
2024-01-30 22:27:10 +0100

    Migrate to cleanuri.com

---
+++ src/services/cleanuri.js
@@ -0,0 +1,13 @@
+const CLEANURI_SHORTENING_API_ENDPOINT = 'https://cleanuri.com/api/v1/shorten'
+
+export const cleanuriShortener = async (url) => {
+  const response = await fetch(CLEANURI_SHORTENING_API_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'url=' + encodeURIComponent(url),
+  })
+  const data = await response.json()
+  return data
+}
